f5b5a36ebb9f2ad92c1bf553a9e57ab1ae0d3bb3
maximeabade <maximeabade>
2024-02-07 10:40:45 +0100

    basis doe for compress and uncompress: compression : miss 1 char at the end, and for uncompress some is lost

--- src/LZ/LZ78.hs
+++ src/LZ/LZ78.hs
@@ -4,11 +4,33 @@
   Maintainer  : ???
 -}
 module LZ.LZ78(compress, uncompress) where
+findPrefix :: String -> [String] -> Maybe Int
+findPrefix prefix dictionary = findPrefix' prefix dictionary 0
+
+findPrefix' :: String -> [String] -> Int -> Maybe Int
+findPrefix' _ [] _ = Nothing
+findPrefix' prefix (x:xs) index
+  | prefix `isPrefixOf` x = Just index
+  | otherwise            = findPrefix' prefix xs (index + 1)
+  where
+    isPrefixOf [] _ = True
+    isPrefixOf _ [] = False
+    isPrefixOf (a:as) (b:bs) = a == b && isPrefixOf as bs
+
+
 
 -- | LZ78 compress method
 compress :: String -> [(Int, Char)]
-compress _ = undefined -- TODO
-compress [] = []
+--compress _ = undefined -- TODO
+compress = compress' [] []
+
+compress' :: [String] -> String -> String -> [(Int, Char)]
+compress' _ _ [] = []  -- Fin de la chaîne d'entrée
+compress' dictionary currentPrefix (x:xs) =
+  case findPrefix (currentPrefix ++ [x]) dictionary of
+    Just index -> compress' dictionary (currentPrefix ++ [x]) xs
+    Nothing    -> (maybe 0 id (findPrefix currentPrefix dictionary), x) : compress' (dictionary ++ [currentPrefix ++ [x]]) [] xs
+
 
 
 
