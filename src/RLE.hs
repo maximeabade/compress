7bfad9829aa5f5ebd4058bf758d58c6e88507735
NoBa <56113460+NobleBa@users.noreply.github.com>
2024-03-05 09:53:45 +0100

    Update RLE.hs

--- src/RLE.hs
+++ src/RLE.hs
@@ -3,13 +3,16 @@
   Description : An implementation of the run-length encoding method
   Maintainer  : ???
 -}
+
 module RLE(compress, uncompress) where
+
+import qualified Data.List as List
 
 -- | RLE compress method
 compress :: Eq a => [a] -> [(a, Int)]
-compress _ = undefined -- TODO
+compress = map(\a -> (head a, length a)) . List.group
 
 -- | RLE uncompress method
 -- If input cannot be uncompressed, returns `Nothing`
 uncompress :: [(a, Int)] -> Maybe [a]
-uncompress _ = undefined -- TODO
+uncompress = concatMap(\(a, n) -> if (n > 0) then replicate n a else Nothing)
