39dba0eb4d53cf3f4a2c90c1c3373bfac5b0278e
maximeabade <maximeabade@gmail.com>
2024-03-11 10:44:38 +0100

    porut

--- src/LZ/LZ78.hs
+++ src/LZ/LZ78.hs
@@ -1,37 +1,48 @@
-{- |
-  Module      : LZ.LZ78
-  Description : An implementation of LZ78 method
-  Maintainer  : MAX
--}
+module LZ.LZ78 where
 
-module LZ.LZ78 (compress, uncompress) where
+import Data.List (isPrefixOf, findIndex, elemIndex, elem, nub)
 
-import Data.List (isPrefixOf)
+findMinPrefix :: String -> [String] -> Maybe Int
+findMinPrefix prefix dictionary =
+  let index = findIndex (isPrefixOf prefix) dictionary
+  in case index of
+    Just i -> if i > 0 then Just (i - 1) else Nothing
+    Nothing -> Nothing
+arrayToString :: [Char] -> String
+arrayToString [] = ""
+arrayToString (x:xs) = x : arrayToString xs
 
--- | Finds the index of the given prefix in the dictionary.
-findPrefix :: String -> [String] -> Maybe Int
-findPrefix prefix dictionary = findPrefix' prefix dictionary 0
-  where
-    findPrefix' _ [] _ = Nothing
-    findPrefix' p (x:xs) index
-      | p `isPrefixOf` x = Just index
-      | otherwise        = findPrefix' p xs (index + 1)
+miseEnforme :: String -> [(Int, Char)]
+miseEnforme [] = []
+miseEnforme (x:xs) = (0, x) : miseEnforme xs
 
--- | LZ78 compress method
-compress :: String -> [(Int, Char)]
-compress "" = []
-compress (x:xs) = (0, x) : compress' [] [x] xs
 
-compress' :: [String] -> String -> String -> [(Int, Char)]
-compress' _ _ [] = []  -- End of input string
-compress' dictionary currentPrefix (x:xs) =
-  case findPrefix (currentPrefix ++ [x]) dictionary of
-    Just index -> compress' dictionary (currentPrefix ++ [x]) xs
-    Nothing
-      | null currentPrefix -> (0, x) : compress' (dictionary ++ [[x]]) [] xs
-      | otherwise -> (maybe 0 id (findPrefix currentPrefix dictionary), x) : compress' (dictionary ++ [currentPrefix ++ [x]]) [x] xs
+estDedans :: (Int, Char) -> [(Int, Char)] -> Bool
+estDedans (i, c) xs = any ((== i) . fst) $ filter ((== c) . snd) xs
 
--- | LZ78 uncompress method (no changes needed here)
+removalDoubles :: [(Int, Char)] -> [(Int, Char)]
+removalDoubles xs = removalDoubles' xs []
+
+removalDoubles' :: [(Int, Char)] -> [(Int, Char)] -> [(Int, Char)]
+removalDoubles' [] acc = reverse acc
+removalDoubles' (x:xs) acc =
+  if estDedans x acc
+    then removalDoubles' xs acc
+    else removalDoubles' xs (x : acc)
+
+--pour chaque (0,char), si estDoublon, alors son index est rajouté au 0 de tous les caracteres qui le suivent ailleurs dans la string
+
+--compress :: String -> [(Int, Char)]
+
+reference :: [(Int, Char)] -> [(Int, Char)] -> [(Int, Char)]
+reference [] _ = []
+reference (x:xs) (y:ys) = if x == y then (0, snd x) : reference xs ys else (fst y + 1, snd y) : reference (x:xs) ys
+
+
+createDictionary :: String -> [(Int, Char)]
+createDictionary [] = []
+createDictionary myString = removalDoubles $ miseEnforme myString         
+
 uncompress :: [(Int, Char)] -> Maybe String
 uncompress [] = Just ""
 uncompress ((0, c):xs) = case uncompress xs of
