4d355366cd9ea2003418508e3281778aa5be5818
maximeabade <maximeabade@gmail.com>
2024-03-20 09:05:51 +0100

    raph va voir les commentaires hehe

--- src/RLE.hs
+++ src/RLE.hs
@@ -14,5 +14,5 @@ compress = map(\a -> (head a, length a)) . List.group
 
 -- | RLE uncompress method
 -- If input cannot be uncompressed, returns `Nothing`
-uncompress :: [(a, Int)] -> Maybe [a] OR Nothing
+uncompress :: [(a, Int)] -> Maybe [a] OR Nothing --raph c etait pourtant pas dur...
 uncompress = concatMap(\(a, n) -> if (n > 0) then replicate n a else Nothing)
