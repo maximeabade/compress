7dc93e09ef4a04f521f95b16542a5e38e3973297
maximeabade <maximeabade@gmail.com>
2024-03-20 15:46:14 +0100

    explosion de la string en format Int, Char OK, uncompress OK, reste a gerer les references

--- src/LZ/LZ78.hs
+++ src/LZ/LZ78.hs
@@ -1,15 +1,29 @@
 module LZ.LZ78(compress, uncompress) where
 
---MAINTAINER: MAXIME ABADE <maximeabade.fr><github.com/maximeabade><abademaxim@cy-tech.fr>
-
-import Data.List (isPrefixOf, findIndex)
+import Data.List (findIndex, isPrefixOf)
 
 -- | LZ78 compress method
 compress :: String -> [(Int, Char)]
-compress "" = []
+compress = compress' []
+    where
+        compress' :: [String] -> String -> [(Int, Char)]
+        compress' _ [] = []
+        compress' dict str@(x:xs) =
+            case findIndex (`isPrefixOf` str) dict of
+                Just ref -> (ref, head xs) : compress' dict xs
+                Nothing  -> (0, x) : compress' (dict ++ [str]) xs
 
 
 -- | LZ78 uncompress method
 uncompress :: [(Int, Char)] -> Maybe String
-uncompress [] = Just ""
-uncompress _ = Nothing
+uncompress = fmap concat . mapM expand
+  where
+    expand (ref, char) = do
+      dict <- uncompress' ref
+      return (dict ++ [char])
+
+uncompress' :: Int -> Maybe String
+uncompress' 0 = Just ""
+uncompress' n = fmap (++ [lastChar]) (uncompress' ref)
+  where
+    (ref, lastChar) = (n - 1, toEnum (n - 1))
