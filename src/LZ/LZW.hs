d8dd90960d38ef16a86cb2ae4d4b19ba74be7e4c
maximeabade <maximeabade@gmail.com>
2024-03-30 16:32:29 +0100

    compress LZW

--- src/LZ/LZW.hs
+++ src/LZ/LZW.hs
@@ -1,43 +1,38 @@
-{- |
-  Module      : LZ.LZW
-  Description : An implementation of LZW method
-  Maintainer  : ???
--}
-
 module LZ.LZW(compress, uncompress) where
-import LZ.Dictionaries
+import Data.Char (chr)
+import Data.List (isPrefixOf)
 
-example = "Je suis tout puissant"
-
--- | LZW compress method
+-- | Fonction de compression LZW
 compress :: String -> [Int]
-compress _ = undefined -- TODO
+compress str = compress' str initDict []
+
+-- | Dictionnaire initial
+initDict :: [(String, Int)]
+initDict = zip (map return ['\0'..'\xFF']) [0..255]
+
+-- | Fonction de compression récursive
+compress' :: String -> [(String, Int)] -> [Int] -> [Int]
+compress' "" _ encoded = encoded
+compress' str dict encoded =
+  let
+    (prefix, rest) = findLongestPrefix str dict
+    (Just newEntry) = lookup prefix dict
+    newEncoded = encoded ++ [newEntry]
+    newDict = if null rest
+              then dict
+              else (prefix ++ [head rest], length dict) : dict
+  in compress' rest newDict newEncoded
+
+-- | Fonction pour trouver la plus longue chaîne préfixée dans un dictionnaire
+findLongestPrefix :: String -> [(String, Int)] -> (String, String)
+findLongestPrefix str dict = go str ""
+  where
+    go [] prefix = (prefix, [])
+    go (c:cs) prefix = case lookup (prefix ++ [c]) dict of
+      Just _ -> go cs (prefix ++ [c])
+      Nothing -> (prefix, c:cs)
 
 -- | LZW uncompress method
--- If input cannot be uncompressed, returns `Nothing`
+-- Si l'entrée ne peut pas être décompressée, retourne `Nothing`
 uncompress :: [Int] -> Maybe String
 uncompress _ = undefined -- TODO
-
-has:: String -> [Char] -> Int
-has word dict = word `elemIndex` dict
- 
-    
-newSequence:: String -> [(Chat,Int)] -> Bool
-newSequence sequence dict
-
-
-compress':: String -> [String] -> [Int]
-compress' text  dict = Nothing
-
-
-{-
-lecture caractère
-
-Si dans Disctionnaire
-   -> Prendre numero de table correspondant
-   -> Letttre suivante 
-
-Si PAs dedans 
-  -> Rajout de la lettre
-
--}
