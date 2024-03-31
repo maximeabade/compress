02eca03270620dac55dd8a6396c4f5dd5418a34f
DrussRek <remcap2@gmail.com>
2024-03-31 16:00:26 +0200

    working Uncompress

--- src/LZ/LZW.hs
+++ src/LZ/LZW.hs
@@ -1,6 +1,7 @@
 module LZ.LZW(compress, uncompress) where
 import Data.Char (chr)
 import Data.List (isPrefixOf)
+import Data.Maybe (fromMaybe)
 import LZ.Dictionaries (empty, ascii, zeroAsChar)
 
 -- | Fonction de compression LZW
@@ -41,18 +42,39 @@ findLongestPrefix str dict = go str ""
 asciiDict :: [(Int, [Char])]
 asciiDict = zip [0..255] (map return ['\0'..'\xFF'])
 
--- Si l'entrée ne peut pas être décompressée, retourne `Nothing`
-uncompress :: [Int] -> Maybe String
-uncompress [] = Just ""
-uncompress code = go code asciiDict ""  -- Utilisez `asciiDict` comme dictionnaire
+
+--- Décompression d'une liste d'entiers en chaîne de caractères
+uncompress :: [Int] -> IO (Maybe String)
+uncompress [] = do
+  putStrLn "Décompression terminée avec succès."
+  return $ Just ""
+
+uncompress code = go 1 code asciiDict ""
   where
-    go [] _ output = Just output
-    go (codeNr:rest) dict output =
+    go _ [] _ output = do
+      putStrLn "Décompression terminée avec succès."
+      return $ Just output
+    go step (codeNr:rest) dict output = do
+      putStrLn $ "Étape " ++ show step ++ ":"
+      putStrLn $ "  Code actuel: " ++ show codeNr
       case lookup codeNr dict of
-        Just phrase -> go rest dict (output ++ phrase)
-        Nothing ->
-          case lookup (head rest) dict of
-            Just nextPhrase ->
-              let newEntry = (length dict, output ++ [head nextPhrase])
-              in go rest (newEntry : dict) (output ++ [head nextPhrase])
-            Nothing -> Nothing
+        Just phrase -> do
+          putStrLn $ "  Phrase trouvée dans le dictionnaire: " ++ show phrase
+          case rest of
+            [] -> go (step + 1) rest dict (output ++ phrase)
+            (nextCode:_) -> do
+              let nextPhrase = fromMaybe [] (lookup nextCode dict)
+              let newEntry = (length dict, phrase ++ [head nextPhrase])
+              putStrLn $ "  Ajout d'une nouvelle entrée au dictionnaire: " ++ show newEntry
+              go (step + 1) rest (dict ++ [newEntry]) (output ++ phrase)
+        Nothing -> do
+          putStrLn $ "  Code non trouvé dans le dictionnaire."
+          case rest of
+            [] -> do
+              putStrLn "  Impossible de décompresser la liste d'entiers."
+              return Nothing
+            (nextCode:_) -> do
+              let nextPhrase = fromMaybe [] (lookup nextCode dict)
+              let newEntry = (length dict, nextPhrase)
+              putStrLn $ "  Ajout d'une nouvelle entrée au dictionnaire: " ++ show newEntry
+              go (step + 1) rest (dict ++ [newEntry]) (output ++ nextPhrase)
