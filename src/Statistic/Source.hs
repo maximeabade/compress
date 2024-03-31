dab4b2255e8ca99c959b59f93138b26b2506359e
maximeabade <maximeabade@gmail.com>
2024-03-31 12:54:27 +0200

    Push de Source.hs

--- src/Statistic/Source.hs
+++ src/Statistic/Source.hs
@@ -1,22 +1,38 @@
-{- |
-  Module : Statistic.Source
-  Description : Some utility functions for sources (input messages)
-  Maintainer : ???
--}
+module Statistic.Source where
 
-module Statistic.Source(occurrences, entropy, orderedCounts) where
+import Data.List (sortBy)
+import Data.Map (Map, fromList, toList)
 
-import Data.Map (Map)
-
--- | The map giving occurrences of each symbol in the source
+-- Function to count symbol occurrences
 occurrences :: Ord a => [a] -> Map a Int
-occurrences _ = undefined -- TODO
+occurrences message =
+  let charCounts = map (\c -> (c, length (filter (== c) message))) (support message)
+  in fromList charCounts
 
--- | SHANNON entropy of source
-entropy :: Ord a => [a] -> Double
-entropy _ = undefined -- TODO
+-- Function to remove duplicates from a list
+support :: Ord a => [a] -> [a]
+support = foldr (\x xs -> if x `elem` xs then xs else x : xs) []
 
 -- | List of occurrences ordered by count
-orderedCounts :: Ord a => [a] -> [(a, Int)]
-orderedCounts _ = undefined -- TODO
+orderedCounts :: Ord a => [(a, Int)] -> [(a, Int)]
+orderedCounts elements =
+  sortBy (\(w1, count1) (w2, count2) -> compare count1 count2) elements
 
+-- Function to calculate Shannon entropy
+shannonEntropy :: Map a Int -> Double
+shannonEntropy counts =
+  let total = fromIntegral (sum (map snd (toList counts)))  -- Total count of symbols
+      probabilities = map (\(_, count) -> fromIntegral count / total) (toList counts)
+      log2Base = logBase 2  -- Or `log2` if available
+      entropy = sum $ map (\p -> if p == 0 then 0 else -p * log2Base p) probabilities
+  in entropy
+
+-- Example usage (assuming main is defined elsewhere)
+main = do
+  let message = "je comprends pas"
+  let counts = occurrences message
+  print counts
+  let result = orderedCounts (toList counts)
+  print result
+  let entropy = shannonEntropy counts
+  print entropy
